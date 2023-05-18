710dbaabc757b747beb9fc46c7609c367a76c5a7
Johnny <victorjon@163.com>
2023-05-18 14:38:13 +0800

    Update SolidityLogTest.sol

--- SolidityLogTest.sol
+++ SolidityLogTest.sol
@@ -113,5 +113,52 @@ contract SolidityLogTest {
 	function getName(string memory eventName) public pure returns (bytes32) {
 		return keccak256(abi.encodePacked(eventName));
 	}
+	
+	
+		
+	/***** General encapsulation for log1, log2, log3 and log for testing *****/
 
+	function exudeLog(uint i, bytes32 eventSig, bytes32 topic1, bytes32  topic2, bytes32 topic3, bytes memory data) public {
+        	 assembly { 
+		 let p := add(data, 0x20)
+		 let s := mload(data) 
+		    if eq(i, 0x1) {
+			log1(p, s, eventSig)  
+		    } 
+		    if eq(i, 0x2) {
+			log2(p, s, eventSig, topic1)  
+		    } 
+		    if eq(i, 0x3) {
+			log3(p, s, eventSig, topic1, topic2)  
+		    }
+		    if eq(i, 0x4) {
+			log4(p, s, eventSig, topic1, topic2, topic3)  
+		    }
+		}
+    	} 
+
+  	function getEventSig(string memory eventSigStr) public pure returns (bytes32) {
+        	return keccak256(abi.encodePacked(eventSigStr));
+    	}
+
+	/***** Example of code snippet for exudeLog() testing *****/
+
+	event Fire(address indexed t1, uint256 abc);
+	event Fire(address indexed t1, uint256 indexed t2, uint256 abc);
+	event Fire(address indexed t1, uint256 indexed t2, bool indexed t3, uint256 abc);
+
+	function test(address t1, uint256 t2, bool t3, uint256 abc) external {
+		emit Fire(t1, t2, t3, abc);
+		exudeLog(4, getEventSig("Fire(address,uint256,bool,uint256)"), bytes32(abi.encode(t1)), bytes32(abi.encode(t2)), bytes32(abi.encode(t3)), abi.encode(abc));
+	}
+	
+	function test(address t1, uint256 t2, uint256 abc) external {
+		emit Fire(t1, t2, abc);
+		exudeLog(3, getEventSig("Fire(address,uint256,uint256)"), bytes32(abi.encode(t1)), bytes32(abi.encode(t2)), bytes32(0), abi.encode(abc));
+	}
+
+	function test(address t1, uint256 abc) external {
+		emit Fire(t1, abc);
+		exudeLog(2, getEventSig("Fire(address,uint256)"), bytes32(abi.encode(t1)), bytes32(0), bytes32(0), abi.encode(abc));
+	}
 }
